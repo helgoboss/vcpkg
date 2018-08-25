8e862089a7259e650c614981b5598e81ef8b9826
Benjamin Klum <benjamin.klum@helgoboss.org>
2018-08-25 17:04:19 +0200

    Use helgoboss build config also for Boost

--- scripts/toolchains/osx.cmake
+++ scripts/toolchains/osx.cmake
@@ -3,39 +3,44 @@ if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
 endif()
 set(CMAKE_SYSTEM_NAME Darwin CACHE STRING "")
 
-# Recreate translation properties that I also used when compiling Boost for VST plugin development
+# The following lines are supposed to produce the correct "helgoboss" build configuration for all packages
+# that vcpkg builds directly via CMake. I will derive the correct "helgoboss" build configuration from the way I used
+# to compile Boost in the past, so the comments will contain b2 command line options. 
+#
+# I used the following b2 command line to build it:
+#
+#         ./b2 -a toolset=clang macosx-version-min=10.7 threading=multi link=static runtime-link=static linkflags="-stdlib=libc++"
+#              cxxflags="-arch i386 -arch x86_64 -std=c++1y -stdlib=libc++ -isysroot /Developer/SDKs/MacOSX10.7.sdk" 
+#
+# Please note that the following lines don't affect Boost builds because vcpkg builds Boost via b2.
+# In order to use "helgoboss" build configuration also for Boost, modifications in the following files might be necessary:
+#
+# - ports/boost-modular-build-helper/CMakeLists.txt
+# - ports/boost-modular-build-helper/boost-modular-build.cmake
+
+# toolset=clang
+# Default
 
 # macosx-version-min=10.7
 set(CMAKE_OSX_DEPLOYMENT_TARGET "10.7" CACHE STRING "")
 
 # threading=multi
-# Probably default
+# Default
 
 # link=static
-# See VCPKG_LIBRARY_LINKAGE in triplet
+# Set via VCPKG_LIBRARY_LINKAGE in triplet
 
 # runtime-link=static
-# See VCPKG_CRT_LINKAGE in triplet
+# Set via VCPKG_CRT_LINKAGE in triplet
 
-# cxxflags="-arch i386 -arch x86_64"
+# cxxflags="-arch i386 -arch x86_64 ...
 set(CMAKE_OSX_ARCHITECTURES "i386;x86_64" CACHE STRING "")
 
-# isysroot /Developer/SDKs/MacOSX10.7.sdk
+# ... -std=c++1y -stdlib=libc++ ...
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1y -stdlib=libc++" CACHE STRING "")
+
+# ... isysroot /Developer/SDKs/MacOSX10.7.sdk" ....
 set(CMAKE_OSX_SYSROOT "/Developer/SDKs/MacOSX10.7.sdk" CACHE STRING "")
 
-
-# To enable C++11/14 support by default for this older SDK we still need to do something
-# (otherwise e.g. uwebsockets doesn't compile because it can't find mutex header)
-
-# Add first try this
-set(CMAKE_CXX_STANDARD 14 CACHE STRING "")
-
-# If it doesn't work, could try something like that:
-
-# -std=c++1y
-# set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++1y" CACHE STRING "")
-
-# -stdlib=libc++
-#set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++" CACHE STRING "")
-
-# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1y -stdlib=libc++" CACHE STRING "")
+# linkflags="-stdlib=libc++"
+# Seemingly redundant
